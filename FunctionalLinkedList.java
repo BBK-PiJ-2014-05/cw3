837800fbd4a4552c29fd690dd5ad6a2f84ca238c
Geoff <geoff777@gmail.com>
2015-01-30 17:45:32 +0000

    cw3 files

---
+++ FunctionalLinkedList.java
@@ -0,0 +1,38 @@
+public class FunctionalLinkedList extends LinkedList implements FunctionalList {
+
+FunctionalList fList;
+
+
+public ReturnObject head() {
+	return get(0);
+
+}
+
+
+public FunctionalList rest() {
+	ListNode currentNode = head;
+
+	ReturnObject fObj;
+	fList = new FunctionalLinkedList();
+	for (int i = 0; i < size(); i++) {
+		if (currentNode.getNext() == null) {
+			return null;
+		}
+		fList.add(currentNode.getNext());
+		currentNode = currentNode.getNext();
+
+
+		fObj = new ReturnObjectImpl(currentNode.getContent());
+		System.out.println("" + fObj.getReturnValue());
+
+
+	}
+
+	return fList;
+
+}
+
+
+
+
+}
