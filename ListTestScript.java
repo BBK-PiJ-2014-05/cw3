19696a3cd096c4e3e9109a3ca135707244daa08a
Geoff <geoff777@gmail.com>
2015-01-30 13:27:50 +0000

    cw3 files

--- ListTestScript.java
+++ ListTestScript.java
@@ -1,39 +1,69 @@
 public class ListTestScript {
 
 public static void main(String[] args) {
+	ListTestScript test = new ListTestScript();
+	test.launch();
 
-	LinkedList link = new LinkedList();
-	link.add("33");
-	link.add("22");
-	link.add("11");
-	link.add("90");
-	link.add("78");
+}
 
-	System.out.println("size is: " + link.size());
-	System.out.print("link.get(1) ");
-	link.get(1);
-	link.remove(1);
-
-	link.get(0);
-	link.get(1);
-	link.get(2);
-	link.get(3);
-	link.add(2,"34");
-	System.out.println("size is: " + link.size());
-	link.get(0);
-	link.get(1);
-	link.get(2);
-	link.get(3);
-	link.get(4);
-	//link.get(5);
-
-	FunctionalList flist = new FunctionalArrayList();
-	flist.add("8766");
-	flist.add("44");
-	flist.get(0);
-
-
+public void launch() {
+	List firstTest = new LinkedList();
+	//TestCase1(firstTest);
+	List secondTest = new ArrayList();
+	//TestCase1 (secondTest);
+	FunctionalList thirdTest = new FunctionalArrayList();
+	TestCase2(thirdTest);
+	FunctionalList fourthTest = new FunctionalLinkedList();
+	TestCase2(fourthTest);
 
 
 }
+
+
+public void TestCase1(List test) {
+
+	test.add("11");
+	test.add("22");
+	test.add("33");
+	test.add("44");
+	test.add("55");
+	test.add("66");
+	System.out.print("Element at position 0: ");
+	test.get(0);
+	System.out.print("Element at position 1: ");
+	test.get(1);
+	System.out.print("Element at position 2: ");
+	test.get(2);
+	test.remove(1);
+	System.out.print("after removing the element at pos 1, new element at pos 1 is: ");
+	test.get(1);
+
+
+}
+
+public void TestCase2(FunctionalList test) {
+
+	test.add("11");
+		test.add("22");
+		test.add("33");
+		test.add("44");
+		test.add("55");
+		test.add("66");
+		System.out.print("Element at position 0: ");
+		test.get(0);
+		System.out.print("Element at position 1: ");
+		test.get(1);
+		System.out.print("Element at position 2: ");
+		test.get(2);
+		test.remove(1);
+		System.out.print("after removing the element at pos 1, new element at pos 1 is: ");
+	test.get(1);
+	test.head();
+	//test.rest();
+
+}
+
+
+
+
 }
