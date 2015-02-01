f8b724797bd28b80a57c47d50105d0aef2c2075d
Geoff <geoff777@gmail.com>
2015-02-01 11:31:43 +0000

    cw3 files

--- ListTestScript.java
+++ ListTestScript.java
@@ -8,19 +8,44 @@ public static void main(String[] args) {
 
 public void launch() {
 	List firstTest = new LinkedList();
-	//TestCase1(firstTest);
+	//ListTest(firstTest);
 	List secondTest = new ArrayList();
-	//TestCase1 (secondTest);
+	//ListTest (secondTest);
 	FunctionalList thirdTest = new FunctionalArrayList();
-	TestCase2(thirdTest);
+	//FunctionTest(thirdTest);
 	FunctionalList fourthTest = new FunctionalLinkedList();
-	TestCase2(fourthTest);
+	//FunctionTest(fourthTest);
+	SampleableList fifthTest = new SampleableListImpl();
+	//SampleTest(fifthTest);
+
+	AbstractStack sixthTest = new StackImpl(secondTest);
+	StackFunctions(sixthTest);
 
 
 }
 
 
-public void TestCase1(List test) {
+public void ListTest(List test) {
+
+	ListFunctions(test);
+}
+
+public void FunctionTest(FunctionalList test) {
+
+	ListFunctions(test);
+	System.out.print(" head is ");
+	test.head();
+	test.rest();
+
+}
+
+public void SampleTest (SampleableList test) {
+	ListFunctions(test);
+	test.sample();
+}
+
+
+public void ListFunctions(List test) {
 
 	test.add("11");
 	test.add("22");
@@ -38,32 +63,24 @@ public void TestCase1(List test) {
 	System.out.print("after removing the element at pos 1, new element at pos 1 is: ");
 	test.get(1);
 
-
 }
 
-public void TestCase2(FunctionalList test) {
+public void StackFunctions(AbstractStack test) {
+	test.push("11");
+	test.push("22");
+	test.push("33");
+	test.push("44");
+	test.push("55");
+	test.push("66");
+	System.out.println("Pushed " + test.size() + " elements to the stack.");
+	System.out.print("Element at the top of the stack: ");
+	test.top();
+	System.out.print("After popping, the top of the stack is: ");
+	test.pop();
+	test.top();
+	System.out.println("...and now there are " + test.size() + " elements on the stack.");
 
-	test.add("11");
-		test.add("22");
-		test.add("33");
-		test.add("44");
-		test.add("55");
-		test.add("66");
-		System.out.print("Element at position 0: ");
-		test.get(0);
-		System.out.print("Element at position 1: ");
-		test.get(1);
-		System.out.print("Element at position 2: ");
-		test.get(2);
-		test.remove(1);
-		System.out.print("after removing the element at pos 1, new element at pos 1 is: ");
-	test.get(1);
-	test.head();
-	//test.rest();
 
 }
-
-
-
 
 }
